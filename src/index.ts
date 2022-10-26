49a0e6a8f537c8ff367df9b4f4d8612a8b6d2ffe
Blizzarus <Expresso1414@yahoo.com>
2022-10-26 16:00:01 -0400

    Host web application locally

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import { time } from "console";
 import { WebSocketServer } from "ws";
+var http=require("http");
+var url = require('url');  
+var fs = require('fs');
+var server = http.createServer(function(request: any, response: any) {
+  fs.readFile("WebClient.html", function(error: any, data: any) {  
+    if (error) {  
+        response.writeHead(404);  
+        response.write(error);  
+        response.end();  
+    } else {  
+        response.writeHead(200, {  
+            'Content-Type': 'text/html'  
+        });  
+        response.write(data);  
+        response.end();  
+    }  
+  });
+});
+server.listen(10118);
+console.log("Http server started");
+
 
 /*         Websocket Server         */
 const wss: WebSocketServer = new WebSocketServer({ port: 10117 });
-console.log("server started");
+console.log("WS server started");
 wss.on("connection", function connection(ws, req) {
   console.log("connected to " + req.socket.remoteAddress?.toString());
 
   ws.on("message", function message(data) {
-    console.log("Recieved message: %s", data.toString());
+    console.log("received: %s", data.toString());
     ws.send("Server says hi!");
   });
 });
