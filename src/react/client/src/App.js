f9e199f1055f58ddd6148aa776db55c279a414a5
Meri <iris8733@gmail.com>
2022-11-23 14:30:13 -0500

    changed route path and name from 'Home' to 'Login'

--- src/react/client/src/App.js
+++ src/react/client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from './components/Home';
+import Login from './components/Login';
 import Game from './components/Game';
 import './App.css';
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
           <Route path="/game" element={<Game/>}></Route>
         </Routes>
       </div>
