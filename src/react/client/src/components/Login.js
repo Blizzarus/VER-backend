af338fd1d94223be7d45bad0e269d18a87ca5032
Meri <iris8733@gmail.com>
2022-11-23 14:28:37 -0500

    renamed 'Home' to 'Login'

--- src/react/client/src/components/Login.js
+++ src/react/client/src/components/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { socket } from '../context/socket'
 import { useNavigate } from 'react-router-dom';
-import { Button, Form, Segment, Grid, Header } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image } from 'semantic-ui-react'
 
-const Home = () => {
+const Login = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState('');
   const [isValid, setValid] = useState(true);
@@ -14,6 +14,7 @@ const Home = () => {
       setValid(false);
     } else {
       socket.emit('send-username', userName);
+      // TODO: require unique?
       navigate('/game');
     }
   };
@@ -28,32 +29,35 @@ const Home = () => {
         }
       }
     >
-      <Header as='h1' content='Join the Game!'/>
+      <Image src='/logo192.png' centered />
+      <Header as='h1' color='teal' textAlign='center'>
+      Join the Game!
+      </Header>
       <Form size='large' onSubmit={handleSubmit}>
-          <Form.Input 
-            fluid 
-            error={!isValid}
-            icon='user' 
-            iconPosition='left' 
-            placeholder='Player Name' 
-            value={userName} 
-            onChange={(e) => { 
-              setUserName(e.target.value);
-              setValid(e.target.value !== '');
-            }}
-          />
-          <Button 
-            color='teal' 
-            fluid 
-            size='large' 
-            type='submit'
-          >
-            Join
-          </Button>
+        <Form.Input 
+          fluid 
+          error={!isValid}
+          icon='user' 
+          iconPosition='left' 
+          placeholder='Player Name' 
+          value={userName} 
+          onChange={(e) => { 
+            setUserName(e.target.value);
+            setValid(e.target.value !== '');
+          }}
+        />
+        <Button 
+          color='teal' 
+          fluid 
+          size='large' 
+          type='submit'
+        >
+          Join
+        </Button>
       </Form>
     </Grid.Column>
   </Grid>
   );
 };
 
-export default Home;
+export default Login;
