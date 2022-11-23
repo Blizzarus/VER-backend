0572f3ab90c2346a1716f812a1a51501fee3d4f0
Meri <iris8733@gmail.com>
2022-11-22 23:54:49 -0500

    added sematic-ui components for form,  validation

--- src/react/client/src/components/Home.js
+++ src/react/client/src/components/Home.js
@@ -1,30 +1,58 @@
 import React, { useState } from 'react';
 import { socket } from '../context/socket'
 import { useNavigate } from 'react-router-dom';
+import { Button, Form, Segment, Grid, Header } from 'semantic-ui-react'
 
 const Home = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState('');
+  const [isValid, setValid] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit('send-username', userName);
-    navigate('/game');
+    if (userName === '') {
+      setValid(false);
+    } else {
+      socket.emit('send-username', userName);
+      navigate('/game');
+    }
   };
+
   return (
-    <form className="home__container" onSubmit={handleSubmit}>
-      <h2 className="home__header">Enter Username: </h2>
-      <input
-        type="text"
-        minLength={2}
-        name="username"
-        id="username"
-        className="username__input"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid.Column 
+      style={
+        {
+          minWidth: 300,
+          maxWidth: '50%'
+        }
+      }
+    >
+      <Header as='h1' content='Join the Game!'/>
+      <Form size='large' onSubmit={handleSubmit}>
+          <Form.Input 
+            fluid 
+            error={!isValid}
+            icon='user' 
+            iconPosition='left' 
+            placeholder='Player Name' 
+            value={userName} 
+            onChange={(e) => { 
+              setUserName(e.target.value);
+              setValid(e.target.value !== '');
+            }}
+          />
+          <Button 
+            color='teal' 
+            fluid 
+            size='large' 
+            type='submit'
+          >
+            Join
+          </Button>
+      </Form>
+    </Grid.Column>
+  </Grid>
   );
 };
 
