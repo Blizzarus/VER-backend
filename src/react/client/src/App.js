7290ea86b499a2556a346ced16cbf29746856f41
Meri <iris8733@gmail.com>
2022-11-28 21:43:13 -0500

    changed routes

--- src/react/client/src/App.js
+++ src/react/client/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { socket } from './context/socket';
+import io from 'socket.io-client';
 import Login from './components/Login';
 import Game from './components/Game';
 import './App.css';
 
+const socket = io('http://localhost:4000');
 function App() {
-
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/game" element={<Game/>}></Route>
+          <Route path="/login" element={<Login socket={socket} />}></Route>
+          <Route path="/" element={<Game socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
